986331b9cf062d0f9e9b6c439b1821e1c765f0b3
benhar-dev <53424421+benhar-dev@users.noreply.github.com>
2024-11-21 14:25:37 +1100

    tidy build and dist folder

--- rollup.config.js
+++ rollup.config.js
@@ -5,74 +5,80 @@ import path from "path";
 import license from "rollup-plugin-license";
 import css from "rollup-plugin-css-only";
 
-export default {
-  input: "./src/index.js",
-  output: [
-    // UMD Build
-    {
-      file: "dist/mobject-litegraph.bundle.js",
-      format: "umd",
-      name: "MobjectLitegraph",
-    },
-    // Minified UMD Build
-    {
-      file: "dist/mobject-litegraph.bundle.min.js",
-      format: "umd",
-      name: "MobjectLitegraph",
-      plugins: [terser()],
-    },
-    // ESM Build
-    {
-      file: "dist/mobject-litegraph.bundle.esm.js",
-      format: "esm",
-    },
-    // Minified ESM Build
-    {
-      file: "dist/mobject-litegraph.bundle.esm.min.js",
-      format: "esm",
-      plugins: [terser()],
-    },
-    // CommonJS Build
-    {
-      file: "dist/mobject-litegraph.bundle.cjs.js",
-      format: "cjs",
-    },
-    // Minified CommonJS Build
-    {
-      file: "dist/mobject-litegraph.bundle.cjs.min.js",
-      format: "cjs",
-      plugins: [terser()],
-    },
-  ],
-  plugins: [
-    nodeResolve(),
-    css({
-      output: "mobject-litegraph.css",
-    }),
-    cssLicenseBanner(),
-    license({
-      sourcemap: true,
-      cwd: process.cwd(),
-
-      banner: {
-        commentStyle: "regular",
-
-        content: () => {
-          return makeLicenseContent();
-        },
+export default [
+  {
+    input: "./src/index.js",
+    output: [
+      // UMD Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.js",
+        format: "umd",
+        name: "MobjectLitegraph",
       },
-      thirdParty: {
-        allow: {
-          test: "MIT",
-          failOnUnlicensed: true,
-          failOnViolation: true,
-        },
+      // Minified UMD Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.min.js",
+        format: "umd",
+        name: "MobjectLitegraph",
+        plugins: [terser()],
       },
-    }),
-  ],
-};
+      // ESM Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.esm.js",
+        format: "esm",
+      },
+      // Minified ESM Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.esm.min.js",
+        format: "esm",
+        plugins: [terser()],
+      },
+      // CommonJS Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.cjs.js",
+        format: "cjs",
+      },
+      // Minified CommonJS Build
+      {
+        file: "dist/js/mobject-litegraph.bundle.cjs.min.js",
+        format: "cjs",
+        plugins: [terser()],
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      css({
+        output: "mobject-litegraph.css",
+      }),
+      includeCombinedLicenseInCssFiles(),
+      license({
+        sourcemap: true,
+        cwd: process.cwd(),
 
-function makeLicenseContent() {
+        banner: {
+          commentStyle: "regular",
+
+          content: () => {
+            return makeCombinedLicenseContent();
+          },
+        },
+        thirdParty: {
+          allow: {
+            test: "MIT",
+            failOnUnlicensed: true,
+            failOnViolation: true,
+          },
+        },
+      }),
+      finallyMoveFile({
+        src: "dist/js/mobject-litegraph.css",
+        dest: "dist/css/mobject-litegraph.css",
+      }),
+    ],
+  },
+];
+
+function makeCombinedLicenseContent() {
   const licenseFile = path.join(__dirname, "LICENSE");
   let licenseContent = fs.readFileSync(licenseFile, "utf-8");
   const thirdPartyLicenseFile = path.join(__dirname, "THIRD-PARTY-LICENSES");
@@ -85,11 +91,11 @@ function makeLicenseContent() {
   return licenseContent;
 }
 
-function cssLicenseBanner() {
+function includeCombinedLicenseInCssFiles() {
   return {
     name: "css-license-banner",
     generateBundle(outputOptions, bundle) {
-      const licenseContent = makeLicenseContent();
+      const licenseContent = makeCombinedLicenseContent();
       const banner = `/*\n${licenseContent}\n*/\n`;
 
       for (const fileName of Object.keys(bundle)) {
@@ -97,6 +103,21 @@ function cssLicenseBanner() {
           bundle[fileName].source = banner + bundle[fileName].source;
         }
       }
+    },
+  };
+}
+
+function finallyMoveFile(options) {
+  return {
+    name: "copy-then-delete",
+    closeBundle() {
+      const destDir = path.dirname(options.dest);
+      if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir, { recursive: true });
+      }
+
+      fs.copyFileSync(options.src, options.dest);
+      fs.unlinkSync(options.src);
     },
   };
 }
